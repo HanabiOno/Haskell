a2f8a00360b59a6e737fa5c4ccda67e2dc830724
HanabiOno <hanabi.ob@gmail.com>
2020-05-24 18:24:29 +0200

    Finished first 4 functions

--- functions.hs
+++ functions.hs
@@ -1,6 +1,27 @@
-module functions where
+module Functions where
 
 -- Sum list
 sum' :: Num a => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
+
+-- all-true one-true
+alltrue' :: [Bool] -> Bool
+alltrue' [] = True
+alltrue' (x:xs) | x == True = alltrue' xs
+                | otherwise = False
+
+onetrue' :: [Bool] -> Bool
+onetrue' [] = False
+onetrue' (x:xs) | x == True = True
+                | otherwise = onetrue' xs
+
+-- cat
+cat' :: [String] -> String
+cat' [] = ""
+cat' (x:xs) = x ++ cat'(xs)
+
+-- inf & sup
+infsup' :: (Num a, Ord a) => [a] -> (a -> a -> a) -> a
+infsup' (n:[]) f = n
+infsup' (n:ns) f = f n (infsup' ns f)
--- functionsSpec.hs
+++ functionsSpec.hs
@@ -1,10 +1,16 @@
-module functionsSpec where
+-- runhaskell FunctionsSpec.hs
+module FunctionsSpec where
 
 import Test.Hspec
-import functions
+-- cabal install hspec --lib
+import Functions
+-- import Main
+import Test.QuickCheck
+-- cabal install QuickCheck --lib
+import Control.Exception (evaluate)
 
-spec :: Spec
-spec = do
+main :: IO ()
+main = hspec $ do
   describe "Sum list" $ do
     it "empty" $ do
       (sum' []) `shouldBe` 0
@@ -12,3 +18,30 @@ spec = do
       (sum' [1]) `shouldBe` 1
     it "more than one item" $ do
       (sum' [6,3,7,4,5]) `shouldBe` 25
+
+  describe "all-true one-true" $ do
+    it "empty" $ do
+      (alltrue' []) `shouldBe` True
+    it "one true" $ do
+      (alltrue' [False,False,False,True]) `shouldBe` False
+    it "all true" $ do
+      (alltrue' [True,True,True,True]) `shouldBe` True
+
+    it "empty" $ do
+      (onetrue' []) `shouldBe` False
+    it "one true" $ do
+      (onetrue' [True,False,False,False]) `shouldBe` True
+    it "all true" $ do
+      (onetrue' [True,True,True,True]) `shouldBe` True
+
+  describe "cat" $ do
+    it "empty" $ do
+      (cat' []) `shouldBe` ""
+    it "hello + + world" $ do
+      (cat' ["hello"," ","world"]) `shouldBe` "hello world"
+
+  describe "infsup" $ do
+    it "max" $ do
+       (infsup' [1..40] max) `shouldBe` 40
+    it "min" $ do
+       (infsup' [1..40] min )`shouldBe` 1
